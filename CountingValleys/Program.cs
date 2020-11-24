09cb5d7369b3a44f71e1467e97ddb31f522db77a
Yusuff Bello K <ykbello2011@gmail.com>
2020-11-24 10:12:01 +0100

    first commit - CountingValleys

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CountingValleys
+{
+    class Program
+    {
+        public static int CountingValleys(int steps, string path){
+            int level = 0, count = 0;
+            bool startedCount = false, balancedCount = false;
+
+            foreach(var p in path){
+                var currentLevel = level;
+                if (p == 'D')
+                    level--;
+                else 
+                    level++;
+                
+                if (currentLevel == 0 && level == -1 || balancedCount)
+                    startedCount = true;
+
+                if (currentLevel == -1 && level == 0 && startedCount){
+                    balancedCount = true;
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountingValleys(8, "UDDDUDUU"));
+        }
+    }
+}
