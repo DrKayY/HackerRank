183867ffc199611c518793b93132fe5bd98f099f
Yusuff Bello K <ykbello2011@gmail.com>
2020-11-25 08:04:44 +0100

    Electronic Shop

---
+++ ElectronicsShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElectronicsShop
+{
+    class Program
+    {
+        static int GetMoneySpent(int[] keyboards, int[] drives, int b)
+        {
+            var totalPricesPossible = new List<int>() {};
+
+            foreach (var k in keyboards) {
+                foreach (var d in drives) {
+                    if ((k + d) <= b)
+                        totalPricesPossible.Add(k + d);
+                }
+            }
+
+            totalPricesPossible.Sort();
+
+            return totalPricesPossible.Count != 0 ? totalPricesPossible[totalPricesPossible.Count - 1] : -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMoneySpent(new int[] {3, 1}, new int[] {5, 2, 8}, 10));
+            Console.WriteLine(GetMoneySpent(new int[] {4}, new int[] {5}, 5));
+        }
+    }
+}
