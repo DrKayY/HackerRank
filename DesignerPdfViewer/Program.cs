6775f82c76684dcb227d4428a8d3426b2a76922f
Yusuff Bello K <ykbello2011@gmail.com>
2021-01-11 10:36:36 +0100

    TheHurdleRace and DesignerPdfViewer

---
+++ DesignerPdfViewer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignerPdfViewer
+{
+    class Program
+    {
+        public static int DesignerPdfViewer(int[] h, string word) {
+            var wordHeightList = new List<int>();
+            foreach(var w in word) {
+                var height = h[(int)w - 97];
+                if(!wordHeightList.Contains(height))
+                    wordHeightList.Add(height);
+            }
+            return wordHeightList.Max() * word.Length;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DesignerPdfViewer(new int[] {1, 2, 1}, "ababab"));
+        }
+    }
+}
---
+++ TheHurdleRace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace TheHurdleRace
+{
+    class Program
+    {
+        public static int HurdleRace(int k, int[] height) {
+            var maxHeight = height.Max();
+            if(maxHeight > k)
+                return maxHeight - k;
+
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HurdleRace(7, new int[] {2, 5, 4, 5, 2}));
+        }
+    }
+}
