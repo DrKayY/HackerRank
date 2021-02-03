ee9a67692718611af7dec91bfe39f9127126a7a8
Yusuff Bello K <ykbello2011@gmail.com>
2021-02-03 11:06:14 +0100

    SequenceEquation_IntroToTutorialChallenges

---
+++ IntroToTutorialChallenges/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntroToTutorialChallenges
+{
+    class Program
+    {
+        public static int IntroTutorial(int V, int[] arr) {
+            var i = 0;
+            while (true) {
+                if (arr[i] == V)
+                    break;
+                i++;
+            }
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IntroTutorial(4, new int[] {1, 4, 5, 7, 9, 12}));
+        }
+    }
+}
---
+++ SequenceEquation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SequenceEquation
+{
+    class Program
+    {
+        public static int[] PermutationEquation(int[] p) {
+            var yArray = new int[p.Length];
+            var aList = new List<int>();
+
+            for (int i = 0 ; i < p.Length ; i++) {
+                aList.Add(p[i]);
+            }
+
+            for (int x = 1 ; x <= p.Length ; x++) {
+                var firstIndex = aList.FindIndex(a => a.Equals(x)) + 1;
+                yArray[x - 1] = aList.FindIndex(a => a.Equals(firstIndex)) + 1;
+            }
+
+            return yArray;
+        }
+        static void Main(string[] args)
+        {
+                foreach (var a in PermutationEquation(new int[] {4, 3, 5, 1, 2})) {
+                    Console.WriteLine(a);
+                }
+        }
+    }
+}
